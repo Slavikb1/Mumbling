57a19398454337686d32b80d6bcfd7fbb25bd149
slavik bidarski <slavikb1@yahoo.com>
2021-12-05 14:07:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Mumbling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string!");
+            string Accum = Console.ReadLine();
+            char[] breakeDown = Accum.ToCharArray();
+
+            for (int i = 0; i < breakeDown.Length; i++)
+            {
+                for (int j = 0; j < i+1; j++)
+                {
+                   if (j==0 && i==0)
+                    {
+                        Console.Write(breakeDown[i].ToString().ToUpper());
+                    }
+                   else if (j == 0)
+                    {
+                        Console.Write(breakeDown[i].ToString().ToUpper());
+                    }
+                    else
+                    {
+                        Console.Write((breakeDown[i]).ToString().ToLower());
+                    }
+                }
+                if (i!= breakeDown.Length-1)
+                {
+                    Console.Write("-");
+                }
+            }
+        }
+    }
+}
